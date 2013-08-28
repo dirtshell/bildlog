e5e2e4ce369b686dfe4d7f27fb3be0b1bf46884e
dirtshell <j.kenin@gmail.com>
2013-08-28 14:49:30 +0000

    Fixed a bug that prevented server execution

--- bildlog/urls.py
+++ bildlog/urls.py
@@ -21,6 +21,6 @@ urlpatterns = patterns('',
     url(r'^login/$', 'bildloguser.views.login_view', name="login"),             # The login page
     url(r'^logout/$', 'bildloguser.views.logout_view', name="logout"),          # The logout page
     url(r'^createBild/$', 'bildcontrols.views.createBild', name="createBild"),  # Bild creation form is submitted here
-    url(r'^createLog/$', 'bildControls.views.createLog', name="createLog"),     # Log creation form is submitted here
+    url(r'^createLog/$', 'bildcontrols.views.createLog', name="createLog"),     # Log creation form is submitted here
     url(r'^(?P<username>\S{,30})/$', 'bildloguser.views.view_user_profile', name="view_user_profile"), # View a users profile (usernames have a max length of 30 chars) THIS  NEEDS TO BE AT THE END
 )
