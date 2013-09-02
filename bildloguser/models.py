8c9e3604139927f6ac1c125ec67c4e8edb8bd940
dirtshell <j.kenin@gmail.com>
2013-09-02 00:50:12 +0000

    Added the 'null=False' parameter to the optional user fields

--- bildloguser/models.py
+++ bildloguser/models.py
@@ -12,16 +12,16 @@ class Language(models.Model):
 class BildLogUser(models.Model):
     user                        =           models.OneToOneField(User)          # The django auth user this profile is tied to
     verified                    =           models.BooleanField(default=0)      # Users must click an email link to verify and post comments
-    personal_site               =           models.CharField(max_length=100)    # Dont know if I want to include this
-    languages                   =           models.ManyToManyField(Language)    # The languages the user knows             
-    job                         =           models.CharField(max_length=50)     # The users occupation
-    country                     =           models.CharField(max_length=50)     # The users country
-    github                      =           models.CharField(max_length=50)     # The users github account
-    bitbucket                   =           models.CharField(max_length=50)     # The users bitbucket account
-    contact                     =           models.EmailField()                 # The email the user would like people to contact him with
-    rep                         =           models.BigIntegerField()            # The users rep
-    followers                   =           models.ManyToManyField('self', related_name='follower', symmetrical=False)  # Followers of the user
-    following                   =           models.ManyToManyField('self', related_name='followed', symmetrical=False)  # Users the user is following
+    personal_site               =           models.CharField(max_length=100, null=True)    # Dont know if I want to include this
+    languages                   =           models.ManyToManyField(Language, null=True)    # The languages the user knows             
+    job                         =           models.CharField(max_length=50, null=True)     # The users occupation
+    country                     =           models.CharField(max_length=50, null=True)     # The users country
+    github                      =           models.CharField(max_length=50, null=True)     # The users github account
+    bitbucket                   =           models.CharField(max_length=50, null=True)     # The users bitbucket account
+    contact                     =           models.EmailField(null=True)                 # The email the user would like people to contact him with
+    rep                         =           models.BigIntegerField(null=True)            # The users rep
+    followers                   =           models.ManyToManyField('self', related_name='follower', symmetrical=False, null=True)  # Followers of the user
+    following                   =           models.ManyToManyField('self', related_name='followed', symmetrical=False, null=True)  # Users the user is following
     
 class BildLogUserForm(forms.Form):
     username                    =           forms.CharField(max_length=30, required=True)
