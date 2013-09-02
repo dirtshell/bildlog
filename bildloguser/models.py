df0cbb526a3cb13fbf6840d1c1e0f2ed22c022c9
dirtshell <j.kenin@gmail.com>
2013-09-02 00:40:54 +0000

    Updated the BildlogUser model so that it contains all the bits needed for a wholesome user.

--- bildloguser/models.py
+++ bildloguser/models.py
@@ -8,6 +8,20 @@ class BildLogUser(models.Model):
     user                        =           models.OneToOneField(User)          # The django auth user this profile is tied to
     verified                    =           models.BooleanField(default=0)      # Users must click an email link to verify and post comments
     personal_site               =           models.CharField(max_length=100)    # Dont know if I want to include this
+    languages                   =           models.ManyToManyField(Language)    # The languages the user knows             
+    job                         =           models.CharField(max_length=50)     # The users occupation
+    country                     =           models.CharField(max_length=50)     # The users country
+    github                      =           models.CharField(max_length=50)     # The users github account
+    bitbucket                   =           models.CharField(max_length=50)     # The users bitbucket account
+    contact                     =           models.EmailField()                 # The email the user would like people to contact him with
+    rep                         =           models.BigIntegerField()            # The users rep
+    followers                   =           models.ManyToManyField('self', related_name='follower', symmetrical=False)  # Followers of the user
+    following                   =           models.ManyToManyField('self', related_name='followed', symmetrical=False)  # Users the user is following
+    
+class Language(models.Model):
+    language                    =           models.CharField(max_length=20, null=False, unique=True) 
+    creation_date               =           models.DateTimeField(auto_now_add = True)
+    last_updated                =           models.DateTimeField(auto_now = True)
     
 class BildLogUserForm(forms.Form):
     username                    =           forms.CharField(max_length=30, required=True)
